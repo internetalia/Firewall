7ca43c359b19df47e73461590f0c620d3ec598e4
Joe <jjoebyrne@gmail.com>
2019-03-19 00:27:37 +0000

    added rules to restrict to cloudflare AND client ip address
    untested

--- src/Firewall/Rules/FirewallRulesEngine.cs
+++ src/Firewall/Rules/FirewallRulesEngine.cs
@@ -66,6 +66,45 @@ namespace Firewall
         }
 
         /// <summary>
+        /// Configures the Firewall to allow requests from IP addresses which belong to Cloudflare.
+        /// </summary>
+        /// <param name="rule">Base rule which gets validated when the request did not come from Cloudflare.</param>
+        /// <param name="allowedClientIPAddresses">The list of client ip addresses proxied through cloudflare</param>
+        /// <param name="ipv4ListUrl">URL which returns a list of all Cloudflare IPv4 address ranges.</param>
+        /// <param name="ipv6ListUrl">URL which returns a list of all Cloudflare IPv6 address ranges.</param>
+        public static IFirewallRule ExceptFromCloudflareAndClientIPs(
+            this IFirewallRule rule,
+            List<IPAddress> allowedClientIPAddresses,
+            string ipv4ListUrl = null,
+            string ipv6ListUrl = null)
+        {
+            var helper = new CloudflareHelper(new HttpClient());
+            var (ips, cidrs) = helper.GetIPAddressRangesAsync(ipv4ListUrl, ipv6ListUrl).Result;
+
+            return new IPAddressRule(new IPAddressRule(new IPAddressRangeRule(rule, cidrs), ips), allowedClientIPAddresses);
+        }
+
+
+        /// <summary>
+        /// Configures the Firewall to allow requests from IP addresses which belong to Cloudflare.
+        /// </summary>
+        /// <param name="rule">Base rule which gets validated when the request did not come from Cloudflare.</param>
+        /// <param name="allowedClientIPAddressRanges">Address ranges of client ips proxied through cloudflare</param>
+        /// <param name="ipv4ListUrl">URL which returns a list of all Cloudflare IPv4 address ranges.</param>
+        /// <param name="ipv6ListUrl">URL which returns a list of all Cloudflare IPv6 address ranges.</param>
+        public static IFirewallRule ExceptFromCloudflareAndClientIPRanges(
+            this IFirewallRule rule,
+            List<CIDRNotation> allowedClientIPAddressRanges,
+            string ipv4ListUrl = null,
+            string ipv6ListUrl = null)
+        {
+            var helper = new CloudflareHelper(new HttpClient());
+            var (ips, cidrs) = helper.GetIPAddressRangesAsync(ipv4ListUrl, ipv6ListUrl).Result;
+
+            return new IPAddressRangeRule(new IPAddressRule(new IPAddressRangeRule(rule, cidrs), ips),allowedClientIPAddressRanges);
+        }
+
+        /// <summary>
         /// Configures the Firewall to allow requests from specific countries.
         /// </summary>
         public static IFirewallRule ExceptFromCountries(
